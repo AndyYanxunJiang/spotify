bb581f6828c4dd51cc2ef3de9bb0978794501813
Andy Yanxun Jiang <ayx.jiang@mail.utoronto.ca>
2024-10-10 16:54:36 -0400

    add artists

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,25 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from spotify
+# Author: Andy Jiang
+# Date: 10 October 2024
+# Contact: ayx.jiang@mail.utoronto.ca
+# Pre-requisites: None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(spotifyr)
+library(readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+radiohead <- get_artist_audio_features("radiohead")
+saveRDS(radiohead, "radiohead.rds")
+the_national <- get_artist_audio_features("the national")
 
+saveRDS(the_national, "the_national.rds")
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_rds(radiohead, "data/01-raw_data/radiohead.rds") 
 
-         
+
